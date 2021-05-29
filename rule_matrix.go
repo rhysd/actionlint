66fde2975501aaa549e90e66f45a58aeacb59419
rhysd <lin90162@yahoo.co.jp>
2021-05-29 11:35:04 +0900

    fix checking 'include' section in matrix

--- rule_matrix.go
+++ rule_matrix.go
@@ -32,61 +32,80 @@ func (rule *RuleMatrix) VisitJobPre(n *Job) {
 		}
 	}
 
-	rule.checkIncludeExclude("include", m.Include, m)
-	rule.checkIncludeExclude("exclude", m.Exclude, m)
+	rule.checkInclude(m)
+	rule.checkExclude(m)
 }
 
-func (rule *RuleMatrix) checkIncludeExclude(sec string, cfgs []map[string]*MatrixCombination, m *Matrix) {
-	if len(cfgs) == 0 {
+func (rule *RuleMatrix) checkMatrixValuesContain(sec string, name string, heystack []*String, needle *String) {
+	for _, s := range heystack {
+		if s.Value == needle.Value {
+			return // found
+		}
+	}
+
+	qs := make([]string, 0, len(heystack))
+	for _, s := range heystack {
+		qs = append(qs, strconv.Quote(s.Value))
+	}
+	rule.errorf(
+		needle.Pos,
+		"%q in \"exclude\" section but it does not exist in matrix %q configuration. available values are %s",
+		needle.Value,
+		name,
+		strings.Join(qs, ", "),
+	)
+}
+
+func (rule *RuleMatrix) checkInclude(m *Matrix) {
+	if len(m.Include) == 0 {
 		return
 	}
 
 	rows := m.Rows
 	if len(rows) == 0 {
-		rule.errorf(m.Pos, "%q section exists but no matrix variation exists", sec)
+		// Note: 'include' section without any matrix is still useful
 		return
 	}
 
-	for _, cfg := range cfgs {
-		for n, c := range cfg {
-			r, ok := rows[n]
+	for _, cfg := range m.Include {
+		for name, kv := range cfg {
+			r, ok := rows[name]
+			if !ok {
+				continue
+			}
+			rule.checkMatrixValuesContain("include", name, r.Values, kv.Value)
+		}
+	}
+}
+
+func (rule *RuleMatrix) checkExclude(m *Matrix) {
+	if len(m.Exclude) == 0 {
+		return
+	}
+
+	rows := m.Rows
+	if len(rows) == 0 {
+		rule.error(m.Pos, "\"exclude\" section exists but no matrix variation exists")
+		return
+	}
+
+	for _, cfg := range m.Exclude {
+		for k, v := range cfg {
+			r, ok := rows[k]
 			if !ok {
 				qs := make([]string, 0, len(rows))
 				for k := range rows {
 					qs = append(qs, strconv.Quote(k))
 				}
 				rule.errorf(
-					c.Key.Pos,
-					"%q in %q section does not exist in matrix. available matrix configurations are %s",
-					n,
-					sec,
+					v.Key.Pos,
+					"%q in \"exclude\" section does not exist in matrix. available matrix configurations are %s",
+					k,
 					strings.Join(qs, ", "),
 				)
 				continue
 			}
-			if !findStringInSequence(r.Values, c.Value) {
-				qs := make([]string, 0, len(r.Values))
-				for _, s := range r.Values {
-					qs = append(qs, strconv.Quote(s.Value))
-				}
-				rule.errorf(
-					c.Value.Pos,
-					"%q in %q section but it does not exist in matrix %q configuration. available values are %s",
-					c.Value.Value,
-					sec,
-					r.Name.Value,
-					strings.Join(qs, ", "),
-				)
-			}
+			rule.checkMatrixValuesContain("exclude", k, r.Values, v.Value)
 		}
 	}
-}
-
-func findStringInSequence(heystack []*String, needle *String) bool {
-	for _, s := range heystack {
-		if s.Value == needle.Value {
-			return true
-		}
-	}
-	return false
 }
