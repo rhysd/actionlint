12856018ba2d7a6adb3851ebf673fb08300fb741
rhysd <lin90162@yahoo.co.jp>
2021-06-07 19:43:53 +0900

    separate type implementation to expr_type.go

--- expr_sema_test.go
+++ expr_sema_test.go
@@ -952,36 +952,3 @@ func TestExprSemanticsCheckerUpdateSteps(t *testing.T) {
 		t.Fatalf("Global variables map was copied when calling UpdateSteps again")
 	}
 }
-
-func TestExprTypesEqual(t *testing.T) {
-	testCases := []struct {
-		ty    ExprType
-		other ExprType
-	}{
-		{NullType{}, StringType{}},
-		{NumberType{}, StringType{}},
-		{BoolType{}, StringType{}},
-		{StringType{}, BoolType{}},
-	}
-
-	for _, tc := range testCases {
-		var l, r ExprType
-
-		l, r = tc.ty, tc.ty
-		if !l.Equals(r) {
-			t.Errorf("%s should equal to %s", l.String(), r.String())
-		}
-		l, r = tc.ty, tc.other
-		if l.Equals(r) {
-			t.Errorf("%s should not equal to %s", l.String(), r.String())
-		}
-		l, r = tc.ty, AnyType{}
-		if !l.Equals(r) {
-			t.Errorf("%s should equal to %s", l.String(), r.String())
-		}
-		l, r = AnyType{}, tc.ty
-		if !l.Equals(r) {
-			t.Errorf("%s should equal to %s", l.String(), r.String())
-		}
-	}
-}
