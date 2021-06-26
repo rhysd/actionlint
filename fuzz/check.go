44132a129a4968e67f49f61d0b323f6b6094d6b0
rhysd <lin90162@yahoo.co.jp>
2021-06-27 01:31:16 +0900

    add fuzz target to run rule checkers

---
+++ fuzz/check.go
@@ -0,0 +1,42 @@
+// +build gofuzz
+package actionlint_fuzz
+
+import "github.com/rhysd/actionlint"
+
+func parseWorkflowPanicFree(data []byte) *actionlint.Workflow {
+	// Avoid Parse() panicking. It panics when go-yaml panics
+	defer func() { recover() }()
+	w, _ := actionlint.Parse(data)
+	return w
+}
+
+func FuzzCheck(data []byte) int {
+	w := parseWorkflowPanicFree(data)
+	if w == nil {
+		return 0
+	}
+
+	rules := []actionlint.Rule{
+		actionlint.NewRuleMatrix(),
+		actionlint.NewRuleCredentials(),
+		actionlint.NewRuleShellName(),
+		actionlint.NewRuleRunnerLabel([]string{}),
+		actionlint.NewRuleEvents(),
+		actionlint.NewRuleJobNeeds(),
+		actionlint.NewRuleAction("."),
+		actionlint.NewRuleEnvVar(),
+		actionlint.NewRuleStepID(),
+		actionlint.NewRuleExpression(),
+	}
+
+	v := actionlint.NewVisitor()
+	for _, rule := range rules {
+		v.AddPass(rule)
+	}
+
+	if err := v.Visit(w); err != nil {
+		return 0
+	}
+
+	return 1
+}
--- fuzz/parse.go
+++ fuzz/parse.go
@@ -1,8 +1,21 @@
+// +build gofuzz
 package actionlint_fuzz
 
 import (
 	"github.com/rhysd/actionlint"
+	"gopkg.in/yaml.v3"
 )
+
+func canParseByGoYAML(data []byte) (ret bool) {
+	defer func() {
+		if err := recover(); err != nil {
+			ret = false
+		}
+	}()
+	var n yaml.Node
+	yaml.Unmarshal(data, &n)
+	return true
+}
 
 func FuzzParse(data []byte) int {
 	if !canParseByGoYAML(data) {
