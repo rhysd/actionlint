56017187b2fc3d46c71378e0f4e4861e838b17a8
rhysd <lin90162@yahoo.co.jp>
2021-07-03 22:57:44 +0900

    remove redundant js.ValueOf calls

--- playground/main.go
+++ playground/main.go
@@ -16,13 +16,13 @@ func fail(err error, when string) {
 	window.Call("showError", err.Error()+" on "+when)
 }
 
-func encodeErrorAsValue(err *actionlint.Error) js.Value {
+func encodeErrorAsMap(err *actionlint.Error) map[string]interface{} {
 	obj := make(map[string]interface{}, 4)
 	obj["message"] = err.Message
 	obj["line"] = err.Line
 	obj["column"] = err.Column
 	obj["kind"] = err.Kind
-	return js.ValueOf(obj)
+	return obj
 }
 
 func lint(source string) interface{} {
@@ -41,7 +41,7 @@ func lint(source string) interface{} {
 
 	ret := make([]interface{}, 0, len(errs))
 	for _, err := range errs {
-		ret = append(ret, encodeErrorAsValue(err))
+		ret = append(ret, encodeErrorAsMap(err))
 	}
 
 	window.Call("onCheckCompleted", js.ValueOf(ret))
