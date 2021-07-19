c02e600309eb5e8aae3cd310e303089dcc53e837
rhysd <lin90162@yahoo.co.jp>
2021-07-19 20:28:37 +0900

    add 'use strict' to wasm_exec.js on copying it from misc/ directory

--- playground/post-install.bash
+++ playground/post-install.bash
@@ -3,10 +3,8 @@
 set -e -o pipefail
 
 rm -rf ./lib
-mkdir ./lib
-mkdir ./lib/css
-mkdir ./lib/css/fonts
-mkdir ./lib/js
+mkdir -p ./lib/css/fonts
+mkdir -p ./lib/js
 
 cp node_modules/codemirror/lib/codemirror.css ./lib/css/
 cp node_modules/codemirror/theme/material-darker.css ./lib/css/
@@ -16,9 +14,11 @@ cp node_modules/devicon/devicon.min.css ./lib/css/
 
 cp node_modules/devicon/fonts/* ./lib/css/fonts/
 
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ./lib/js/
 cp node_modules/codemirror/lib/codemirror.js ./lib/js/
 cp node_modules/codemirror/addon/selection/active-line.js ./lib/js/
 cp node_modules/codemirror/mode/yaml/yaml.js ./lib/js/
 cp node_modules/ismobilejs/dist/isMobile.min.js ./lib/js/
-cp node_modules/pako/dist/pako.min.js ./lib/js
+cp node_modules/pako/dist/pako.min.js ./lib/js/
+
+echo '"use strict";' > ./lib/js/wasm_exec.js
+cat "$(go env GOROOT)/misc/wasm/wasm_exec.js" >> ./lib/js/wasm_exec.js
