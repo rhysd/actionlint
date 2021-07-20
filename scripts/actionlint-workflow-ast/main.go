e04073cb83d8e7319129d520da52156b0421ff7a
rhysd <lin90162@yahoo.co.jp>
2021-07-20 19:52:51 +0900

    add test for actionlint-workflow-ast tool

--- scripts/actionlint-workflow-ast/main.go
+++ scripts/actionlint-workflow-ast/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 
@@ -9,28 +10,33 @@ import (
 	"github.com/rhysd/actionlint"
 )
 
-func main() {
+func run(args []string, stdin io.Reader, stdout, stderr io.Writer) int {
 	var src []byte
 	var err error
-	if len(os.Args) <= 1 {
-		src, err = ioutil.ReadAll(os.Stdin)
+	if len(args) <= 1 {
+		src, err = ioutil.ReadAll(stdin)
 	} else {
-		if os.Args[1] == "-h" || os.Args[1] == "-help" || os.Args[1] == "--help" {
-			fmt.Println("Usage: go run ./scripts/actionlint-workflow-ast {workflow_file}")
-			os.Exit(0)
+		if args[1] == "-h" || args[1] == "-help" || args[1] == "--help" {
+			fmt.Fprintln(stdout, "Usage: go run ./scripts/actionlint-workflow-ast {workflow_file}")
+			return 0
 		}
-		src, err = ioutil.ReadFile(os.Args[1])
+		src, err = ioutil.ReadFile(args[1])
 	}
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		fmt.Fprintln(stderr, err)
+		return 1
 	}
 	w, errs := actionlint.Parse(src)
 	if len(errs) > 0 {
 		for _, err := range errs {
-			fmt.Fprintln(os.Stderr, err)
+			fmt.Fprintln(stderr, err)
 		}
-		os.Exit(1)
+		return 1
 	}
-	pretty.Println(w)
+	pretty.Fprintf(stdout, "%# v\n", w)
+	return 0
+}
+
+func main() {
+	os.Exit(run(os.Args, os.Stdin, os.Stdout, os.Stderr))
 }
