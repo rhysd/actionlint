d116f014b35063c14f91bd04b0cab7336a7d525e
rhysd <lin90162@yahoo.co.jp>
2023-01-18 21:57:12 +0900

    add `vars` to context availability

--- availability.go
+++ availability.go
@@ -12,33 +12,33 @@ package actionlint
 func WorkflowKeyAvailability(key string) ([]string, []string) {
 	switch key {
 	case "jobs.<job_id>.outputs.<output_id>":
-		return []string{"env", "github", "inputs", "job", "matrix", "needs", "runner", "secrets", "steps", "strategy"}, []string{}
+		return []string{"env", "github", "inputs", "job", "matrix", "needs", "runner", "secrets", "steps", "strategy", "vars"}, []string{}
 	case "jobs.<job_id>.steps.continue-on-error", "jobs.<job_id>.steps.env", "jobs.<job_id>.steps.name", "jobs.<job_id>.steps.run", "jobs.<job_id>.steps.timeout-minutes", "jobs.<job_id>.steps.with", "jobs.<job_id>.steps.working-directory":
-		return []string{"env", "github", "inputs", "job", "matrix", "needs", "runner", "secrets", "steps", "strategy"}, []string{"hashfiles"}
+		return []string{"env", "github", "inputs", "job", "matrix", "needs", "runner", "secrets", "steps", "strategy", "vars"}, []string{"hashfiles"}
 	case "jobs.<job_id>.container.env.<env_id>", "jobs.<job_id>.services.<service_id>.env.<env_id>":
-		return []string{"env", "github", "inputs", "job", "matrix", "needs", "runner", "secrets", "strategy"}, []string{}
+		return []string{"env", "github", "inputs", "job", "matrix", "needs", "runner", "secrets", "strategy", "vars"}, []string{}
 	case "jobs.<job_id>.environment.url":
-		return []string{"env", "github", "inputs", "job", "matrix", "needs", "runner", "steps", "strategy"}, []string{}
+		return []string{"env", "github", "inputs", "job", "matrix", "needs", "runner", "steps", "strategy", "vars"}, []string{}
 	case "jobs.<job_id>.steps.if":
-		return []string{"env", "github", "inputs", "job", "matrix", "needs", "runner", "steps", "strategy"}, []string{"always", "cancelled", "failure", "hashfiles", "success"}
+		return []string{"env", "github", "inputs", "job", "matrix", "needs", "runner", "steps", "strategy", "vars"}, []string{"always", "cancelled", "failure", "hashfiles", "success"}
 	case "jobs.<job_id>.container", "jobs.<job_id>.container.credentials", "jobs.<job_id>.services.<service_id>.credentials":
-		return []string{"env", "github", "inputs", "matrix", "needs", "secrets", "strategy"}, []string{}
+		return []string{"env", "github", "inputs", "matrix", "needs", "secrets", "strategy", "vars"}, []string{}
 	case "jobs.<job_id>.defaults.run":
-		return []string{"env", "github", "inputs", "matrix", "needs", "strategy"}, []string{}
-	case "concurrency", "on.workflow_call.inputs.<inputs_id>.default", "run-name":
-		return []string{"github", "inputs"}, []string{}
+		return []string{"env", "github", "inputs", "matrix", "needs", "strategy", "vars"}, []string{}
 	case "on.workflow_call.outputs.<output_id>.value":
-		return []string{"github", "inputs", "jobs"}, []string{}
+		return []string{"github", "inputs", "jobs", "vars"}, []string{}
 	case "jobs.<job_id>.env", "jobs.<job_id>.secrets.<secrets_id>":
-		return []string{"github", "inputs", "matrix", "needs", "secrets", "strategy"}, []string{}
+		return []string{"github", "inputs", "matrix", "needs", "secrets", "strategy", "vars"}, []string{}
 	case "jobs.<job_id>.concurrency", "jobs.<job_id>.continue-on-error", "jobs.<job_id>.environment", "jobs.<job_id>.name", "jobs.<job_id>.runs-on", "jobs.<job_id>.services", "jobs.<job_id>.timeout-minutes", "jobs.<job_id>.with.<with_id>":
-		return []string{"github", "inputs", "matrix", "needs", "strategy"}, []string{}
+		return []string{"github", "inputs", "matrix", "needs", "strategy", "vars"}, []string{}
 	case "jobs.<job_id>.strategy":
-		return []string{"github", "inputs", "needs"}, []string{}
+		return []string{"github", "inputs", "needs", "vars"}, []string{}
 	case "jobs.<job_id>.if":
-		return []string{"github", "inputs", "needs"}, []string{"always", "cancelled", "failure", "success"}
+		return []string{"github", "inputs", "needs", "vars"}, []string{"always", "cancelled", "failure", "success"}
 	case "env":
-		return []string{"github", "inputs", "secrets"}, []string{}
+		return []string{"github", "inputs", "secrets", "vars"}, []string{}
+	case "concurrency", "on.workflow_call.inputs.<inputs_id>.default", "run-name":
+		return []string{"github", "inputs", "vars"}, []string{}
 	default:
 		return nil, nil
 	}
