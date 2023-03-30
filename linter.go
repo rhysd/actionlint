248ae5dd5bae818bc3077e78964a9f04273c397b
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2023-03-30 10:09:23 +0200

    Fatal error if shellcheck or pyflakes commands are not found

--- linter.go
+++ linter.go
@@ -504,7 +504,8 @@ func (l *Linter) check(
 			if err == nil {
 				rules = append(rules, r)
 			} else {
-				l.log("Rule \"shellcheck\" was disabled:", err)
+				l.log("Error in rule \"shellcheck\":", err)
+				return nil, err
 			}
 		} else {
 			l.log("Rule \"shellcheck\" was disabled since shellcheck command name was empty")
@@ -514,7 +515,8 @@ func (l *Linter) check(
 			if err == nil {
 				rules = append(rules, r)
 			} else {
-				l.log("Rule \"pyflakes\" was disabled:", err)
+				l.log("Error in rule \"pyflakes\":", err)
+				return nil, err
 			}
 		} else {
 			l.log("Rule \"pyflakes\" was disabled since pyflakes command name was empty")
