6df960d98140bf3a074b1b77ac961522e98101ab
hugo-syn <hugo.vincent@synacktiv.com>
2023-08-04 18:21:12 +0200

    restore most of the tests

--- expr_insecure_test.go
+++ expr_insecure_test.go
@@ -292,7 +292,7 @@ func TestExprInsecureNoUntrustedValue(t *testing.T) {
 		"matrix.event.issue.title",
 		"github",
 		"github.event.issue",
-		//"github.event.commits.foo.message",
+		"github.event.commits.foo.message",
 		"github.event.commits[0]",
 		"github.event.commits.*",
 		"github.event.commits.*.foo",
