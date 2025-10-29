4cbd2a51b088f99fcffd28d08aef6f4b0f0f4690
Connor Storer <cestorer@users.noreply.github.com>
2025-10-28 21:49:17 -0500

    Add support for new 1vcpu `ubuntu-slim` runner label

--- rule_runner_label.go
+++ rule_runner_label.go
@@ -35,6 +35,7 @@ var allGitHubHostedRunnerLabels = []string{
 	"windows-2025",
 	"windows-2022",
 	"windows-11-arm",
+	"ubuntu-slim",
 	"ubuntu-latest",
 	"ubuntu-latest-4-cores",
 	"ubuntu-latest-8-cores",
